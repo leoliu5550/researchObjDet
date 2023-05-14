c101bf303ffa9f77d8db5dd74fa30c84eaaf7c8e
Leo <leoliu5550@gmail.com>
2023-05-14 15:18:48 +0800

    adding logging function

--- temp.py
+++ temp.py
@@ -1,12 +1,3 @@
-import yaml
-
-
-with open('hyperparameter.yaml', 'r') as file:
-    data = yaml.safe_load(file)
-print(data)
-
-
-
 # print(yaml.dump(data))
 # print('---')
 # print(type(data))
@@ -18,3 +9,30 @@ print(data)
 # print(dir(torchvision.models))
 
 # print(getattr(torchvision.models, name))
+
+
+import os,logging
+
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# set two handlers
+log_file = "{}.log".format(__name__)
+# rm_file(log_file)
+fileHandler = logging.FileHandler(os.path.join('log', log_file), mode = 'w')
+fileHandler.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+
+# set formatter
+formatter = logging.Formatter('[%(asctime)s] {%(module)s:%(lineno)d} %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+consoleHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+
+# add
+logger.addHandler(fileHandler)
+logger.addHandler(consoleHandler)
+
+logger.info("test95195195151")
--- train.py
+++ train.py
@@ -1,12 +1,38 @@
-import yaml
+#region import_part
+import yaml,os,logging,datetime
 import torch
 import torch.nn as nn
 import torchvision.models as models
 from collections import OrderedDict
+#endregion
 
+#region logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+# set two handlers
+log_file = "{}.log".format(__name__)
+# rm_file(log_file)
+fileHandler = logging.FileHandler(os.path.join('log', datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.log' ), mode = 'w')
+fileHandler.setLevel(logging.DEBUG)
+consoleHandler = logging.StreamHandler()
+consoleHandler.setLevel(logging.DEBUG)
+
+# set formatter
+formatter = logging.Formatter('[%(asctime)s] {%(module)s:%(lineno)d} %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+consoleHandler.setFormatter(formatter)
+fileHandler.setFormatter(formatter)
+
+# add
+logger.addHandler(fileHandler)
+logger.addHandler(consoleHandler)
+
+# logger.info("test")
+#endregion
+
+CFG_PATH = 'cfg/hyperparameter.yaml'
 
 def get_hyper():
-    with open('hyperparameter.yaml', 'r') as file:
+    with open(CFG_PATH, 'r') as file:
         para = yaml.safe_load(file)
     return para
 
