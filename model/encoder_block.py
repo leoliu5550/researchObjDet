41094dec6eedec2cc7b7bd905b5531f2745166ad
Leo <leoliu5550@gmail.com>
2023-07-17 23:07:33 +0800

    加入position encoding 但是測試尚未完成

--- model/encoder_block.py
+++ model/encoder_block.py
@@ -1,7 +1,12 @@
+import sys
+sys.path.append('.')
 import torch
 import torch.nn as nn
 from model.selfattention import multhead
-class encoder_block:
+from model.position_encoding import PositionEmbeddingSine
+
+
+class encoder_block(nn.Module):
     def __init__(self,num_head,d_model):
         super().__init__()
         self.num_head = num_head
@@ -15,6 +20,7 @@ class encoder_block:
             nn.Linear(self.d_model,self.d_model),
             nn.ReLU()
             )
+        
 
     def forward(self,x):
         x_ = x
@@ -25,3 +31,11 @@ class encoder_block:
         x = self.normlayer(x)
 
         return x
+    
+D_MODEL = 6
+NUM_HEAD = 2
+
+# batch,rows,d_model
+x = torch.ones([9,3,D_MODEL])
+model = encoder_block(NUM_HEAD,D_MODEL)
+print(model(x))
--- model/selfattention.py
+++ model/selfattention.py
@@ -1,30 +1,41 @@
 import torch 
+import sys
+sys.path.append('.')
 import torch.nn as nn
+from model.position_encoding import PositionEmbeddingSine
 
 
 class selfattention(nn.Module):
     def __init__(self):
         super().__init__()
+        # self.postin = PositionEmbeddingSine()
 
     def forward(self,q_vale,k_vale,v_vale):
         batch_size, length,heads, d_tensor = k_vale.size()
+        # q_vale = self.postin(q_vale)
+        # k_vale = self.postin(k_vale)
         a_vale = q_vale @ k_vale.permute(0,1,3,2)  / torch.sqrt(torch.tensor(d_tensor))
         b_vale = a_vale @ v_vale
         return b_vale
     
-class multhead(nn.Module):
+class multhead_position(nn.Module):
     def __init__(self,num_head,d_model):
         super().__init__()
         self.num_head = num_head
         self.d_model = d_model
         self.selfatten = selfattention()
+        self.postin = PositionEmbeddingSine(self.d_model)
         self.w_q = nn.Linear(self.d_model,self.d_model)
         self.w_k = nn.Linear(self.d_model,self.d_model)
         self.w_v = nn.Linear(self.d_model,self.d_model)
 
     def forward(self,x):
-        q_vale = self.split(self.w_q(x))
-        k_vale = self.split(self.w_k(x))
+
+        w_q = self.postin(self.w_q(x))
+        w_k = self.postin(self.w_k(x))
+
+        q_vale = self.split(w_q)
+        k_vale = self.split(w_k)
         v_vale = self.split(self.w_v(x))
         x = self.selfatten(q_vale,k_vale,v_vale)
         x = self.concat(x)
